844482b1bc7e46a6f25bb8f8cdc96b9c11bb9332
Anton Hristozov <anton.hristozov@gmail.com>
2022-01-27 14:03:24 -0500

    Adding a script that can be used to run px4 with jmavsim

---
+++ run_px4.sh
@@ -0,0 +1,1 @@
+/home/anton/projects/PX4-Autopilot/Tools/sitl_run.sh /home/anton/projects/PX4-Autopilot/build/px4_sitl_default/bin/px4 none jmavsim none none /home/anton/projects/PX4-Autopilot /home/anton/projects/PX4-Autopilot/build/px4_sitl_default
